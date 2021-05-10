5b3e9fdde1014ac82b5ad225686e83b904f0373e
xbl <x_bl@sina.com>
2021-05-10 23:15:53 +0800

    feat: 获取submodule 名字

--- src/submodule.js
+++ src/submodule.js
@@ -1,5 +1,14 @@
 const getSubmoduleList = function(str) {
-    return [];
+    if (!str) return [];
+    const prefix = '[submodule "';
+    const suffix = '"]';
+    const repleaced = str.substring(str.indexOf(prefix) + prefix.length, str.lastIndexOf(suffix));
+    const obj = {
+        name: repleaced,
+    };
+    const result = [];
+    result.push(obj);
+    return result;
 };
 
 exports.getSubmoduleList = getSubmoduleList;
--- test/submodule.spce.js
+++ test/submodule.spce.js
@@ -4,5 +4,17 @@ const { getSubmoduleList } = require('../src/submodule');
 it('Given 空字符串，When 调用 getSubmoduleList，Then 返回空数组 ', () => {
     const result = getSubmoduleList('');
     assert.equal(Array.isArray(result), true);
-    assert.equal(result.length, 0)
+    assert.equal(result.length, 0);
+});
+
+it('Given 正确submodule 字符串，When 调用 getSubmoduleList，Then 返回 包含submodule 名字的对象数组 ', () => {
+    const result = getSubmoduleList(`
+    [submodule "leg"]
+        path = leg
+        url = https://git.oschina.net/gaofeifps/leg.git
+    `);
+
+    assert.equal(Array.isArray(result), true);
+    assert.equal(result.length, 1);
+    assert.equal(result[0].name, 'leg');
 });
