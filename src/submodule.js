ee09bbb984893190395e6701b29ace324dc94f9e
xbl <x_bl@sina.com>
2021-05-10 23:25:19 +0800

    feat: 获得完整对象

--- src/submodule.js
+++ src/submodule.js
@@ -1,13 +1,24 @@
 const getSubmoduleList = function(str) {
-    if (!str) return [];
+    const result = [];
+    if (!str) return result;
     const prefix = '[submodule "';
     const suffix = '"]';
-    const repleaced = str.substring(str.indexOf(prefix) + prefix.length, str.lastIndexOf(suffix));
-    const obj = {
-        name: repleaced,
-    };
-    const result = [];
-    result.push(obj);
+    const pathPrefix = 'path = ';
+    const urlPrefix = 'url = ';
+    const lines = str.split('\n');
+    for(let i = 0; i < lines.length; i++) {
+        const line = lines[i];
+        if (!line.includes(prefix)) continue ;
+        const name = line.substring(line.indexOf(prefix) + prefix.length, line.lastIndexOf(suffix));
+        const path = lines[i + 1].trim().replace(pathPrefix, '');
+        const url = lines[i + 2].trim().replace(urlPrefix, '');
+        const obj = {
+            name,
+            path,
+            url
+        };
+        result.push(obj);
+    }
     return result;
 };
 
--- test/submodule.spce.js
+++ test/submodule.spce.js
@@ -18,3 +18,18 @@ it('Given 正确submodule 字符串，When 调用 getSubmoduleList，Then 返回
     assert.equal(result.length, 1);
     assert.equal(result[0].name, 'leg');
 });
+
+it('Given 正确submodule 字符串，When 调用 getSubmoduleList，Then 返回 包含submodule 的对象数组 ', () => {
+    const result = getSubmoduleList(`
+    [submodule "leg"]
+        path = pathleg
+        url = https://git.oschina.net/gaofeifps/leg.git
+    `);
+
+    assert.equal(Array.isArray(result), true);
+    assert.equal(result.length, 1);
+    const submodule = result[0];
+    assert.equal(submodule.name, 'leg');
+    assert.equal(submodule.path, 'pathleg');
+    assert.equal(submodule.url, 'https://git.oschina.net/gaofeifps/leg.git');
+});
