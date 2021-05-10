56ef8287d477486392c641d4f9bbf54a523f19c1
xbl <x_bl@sina.com>
2021-05-10 23:27:00 +0800

    feat: 多行submodule

--- test/submodule.spce.js
+++ test/submodule.spce.js
@@ -33,3 +33,21 @@ it('Given 正确submodule 字符串，When 调用 getSubmoduleList，Then 返回
     assert.equal(submodule.path, 'pathleg');
     assert.equal(submodule.url, 'https://git.oschina.net/gaofeifps/leg.git');
 });
+
+it('Given 多行submodule 字符串，When 调用 getSubmoduleList，Then 返回 包含submodule 的对象数组 ', () => {
+    const result = getSubmoduleList(`
+    [submodule "leg"]
+        path = pathleg
+        url = https://git.oschina.net/gaofeifps/leg.git
+    [submodule "leg1"]
+        path = pathleg1
+        url = https://git.oschina.net/gaofeifps/leg1.git
+    `);
+
+    assert.equal(Array.isArray(result), true);
+    assert.equal(result.length, 2);
+    const submodule = result[1];
+    assert.equal(submodule.name, 'leg1');
+    assert.equal(submodule.path, 'pathleg1');
+    assert.equal(submodule.url, 'https://git.oschina.net/gaofeifps/leg1.git');
+});
